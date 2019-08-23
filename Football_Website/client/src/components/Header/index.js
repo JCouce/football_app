ba2fbfdfcfa1b12b0d1e98738a274017c2e033bc
Javier <jcoucelanga@gmail.com>
2019-08-23 16:57:37 +0200

    navbar active element and some css

--- Football_Website/client/src/components/Header/index.js
+++ Football_Website/client/src/components/Header/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './header.css';
 
 export class Header extends Component {
+    selectTab = () =>{
+
+    }
     render() {
         return (
             <div className="headerWrapper">
-                <Link to={'/'} className="headerItem item1 selected">Logo</Link>
-                <Link to={'/'} className="headerItem item2">Go Home</Link>
-                <Link to={'/teams'} className="headerItem item3">Teams</Link>
+                <div className="item1"></div>
+                <NavLink activeClassName="selected" exact to={'/'} className="headerItem item2">Go Home</NavLink>
+                <NavLink activeClassName="selected" to={'/teams'} className="headerItem item3">Teams</NavLink>
             </div>
         );
     }
